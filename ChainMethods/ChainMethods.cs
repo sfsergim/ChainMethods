91d9b0f6fa469a57e4146d9658de5f19b6c54d3d
Sergio Ferr <sfsergim@gmail.com>
2019-10-31 23:03:40 -0300

    Add files via upload

---
+++ ChainMethods/ChainMethods.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AutoAmorAmorDoAlto
+{
+    public class ChainMethods
+
+    {
+        public static void Main()
+        {
+            var my = new StringWorkaround();
+            my.add("Sergio ").add("Ferreira..¢Éª◙").remove().remove().remove().remove().remove();
+            Console.WriteLine(my.print());
+            Console.ReadKey();
+        }
+    }
+
+    public class StringWorkaround
+    {
+        private string _content = "";
+
+
+        public StringWorkaround add(string world)
+        {
+            _content += world;
+            return this;
+        }
+        public StringWorkaround remove()
+        {
+            _content = _content.Remove(_content.Length - 1, 1);
+            return this;
+        }
+        public string print()
+        {
+            return _content;
+        }
+    }
+}
